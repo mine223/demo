934c26d9fa51c92ec75af92098f09daa17a15508
mine223 <mine.lyf223@gmail.com>
2021-06-12 14:27:02 +0545

    Create website for the company, Add Home Page Section for the website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function timedText() {
+    'use strict';
+    var x = document.getElementById('changeText'),
+        index = 0,
+    arry = ['Planning', 'Desinging', 'Development'];
+
+    setInterval(function () {
+
+        x.innerHTML = arry[index];
+        index += 1;
+        if (index >= arry.length) {
+            index = 0;
+        }
+
+
+    }, 5000);
+
+
+}
+
+timedText();
