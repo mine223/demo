f6f6c61aa9f2279c2e637367e4b4a702a72fcfc9
mine223 <mine.lyf223@gmail.com>
2021-06-15 14:27:20 +0545

    Add Responsiveness to the website

--- js/script.js
+++ js/script.js
@@ -19,15 +19,55 @@ function timedText() {
 
 timedText();
 
+//Checking window size
+var x = window.matchMedia("(max-width: 774px)");
+var y = window.matchMedia("(max-width: 541px)");
 
-$('.slider').owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-})
+if (x.matches && !(y.matches)) {
+    $('.slider').owlCarousel({
+        items: 2,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+    });
+} else if (y.matches) {
+    $('.slider').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+    });
+} else {
+    $('.slider').owlCarousel({
+        items: 3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+    });
+}
+
+//Nav bar Scrolling
+$(function () {
+    'use strict';
+    $(document).scroll(function () {
+        var $nav = $('.nav');
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
 
 
-//$(function() {
-//    /\\ 
-//})
+var bar = document.getElementById('bar');
+var menu = document.getElementById('menu-bar');
+
+menu.style.right = "-360px";
+
+bar.onclick = function () {
+    if (menu.style.right == "-360px") {
+        menu.style.right = 0;
+    } else {
+        menu.style.right = "-360px";
+    }
+}
